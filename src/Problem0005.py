483876d9ab43e282bb7664169a71c8397769c6d4
Ryan Rohrer <ryan.rohrer@gmail.com>
2014-05-19 14:49:26 -0700

    used the sexy functional library
    LCM was the way to solve this up front. I'm dumb.

--- src/Problem0005.py
+++ src/Problem0005.py
@@ -1,11 +1,5 @@
-x = 20
-while True:
-  found = True
-  for y in range(1, 21):
-    if x % y != 0:
-      found = False
-      break;
-  if found:
-    print x
-    break;
-  x += 1
+import fractions
+def lcm(x, y):
+  return (x*y) / fractions.gcd(x,y)
+
+print reduce(lcm, list(range(1,20)))
