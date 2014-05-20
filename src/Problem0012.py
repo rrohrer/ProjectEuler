bff1064c1262a21dad6b2464ce055e2b95183944
Ryan Rohrer <ryan.rohrer@gmail.com>
2014-05-20 15:07:57 -0700

    12 solved
    doesn't pass the 3 second test, takes 7 to run

--- src/Problem0012.py
+++ src/Problem0012.py
@@ -1,14 +1,15 @@
 def generate_n_triangle_num(n):
-  return sum((range(1, n+1)))
+  return (n * (n + 1)) / 2
 
 def count_divisors(n):
-  return len(filter(lambda x: n % x == 0, (range(1, n + 1))))
+  root = int(n ** 0.5)
+  return len(filter(lambda x: n % x == 0, (range(1, root)))) * 2
 
 n = 0
 while True:
   n += 1
   div_count = count_divisors(generate_n_triangle_num(n))
-  print div_count
+
   if div_count >= 500:
     break
 
