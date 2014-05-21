943abd33d84d7a2719d4f1cfd3fa55384beb4dfa
Ryan Rohrer <ryan.rohrer@gmail.com>
2014-05-21 14:56:24 -0700

    used dynamic programming to solve the problem
    way faster. turns out this problem has an optimal substructure. Meaning
    it can be broken up in to smaller parts. aka pathfinding

--- src/Problem0015.py
+++ src/Problem0015.py
@@ -9,5 +9,17 @@ def n_n_solve(x, y, width, height):
     global hit_counter
     hit_counter += 1
 
-n_n_solve(0, 0, 20, 20)
-print hit_counter
+#n_n_solve(0, 0, 20, 20)
+#print hit_counter
+
+
+def dynamic_progaming_solve(w, h):
+  width = w + 1
+  height = h + 1
+  grid = [1] * width * height
+  for x in range(1, width):
+    for y in range(1, height):
+      grid[width * y + x] = grid[width * y + (x - 1)] + grid[width * (y - 1) + x]
+  return grid[-1]
+
+print dynamic_progaming_solve(20, 20)
