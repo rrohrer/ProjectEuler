c9cef617a1b1158129a0801cd7cdffdcc7acd6a2
Ryan Rohrer <ryan.rohrer@gmail.com>
2014-05-21 17:57:46 -0700

    started 17, obvs doesnt run

---
+++ src/Problem0017.py
@@ -0,0 +1,6 @@
+ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+tens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eightteen", "nineteen"]
+other_tens = ["twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+hundred = "hundred"
+an = "and"
+
